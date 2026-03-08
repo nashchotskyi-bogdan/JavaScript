c62135e23de6fdcf3b72db4a6c878dfeaec2b77c
Bogdan <naszczotskyib@gmail.com>
2026-03-08 22:08:46 +0100

    JS_1

---
+++ 1/main.js
@@ -0,0 +1,8 @@
+let userAgeValue = 16;
+let userNameNickname = "Bogdan";
+
+userAgeValue = 17;
+userNameNickname = "BohdanDev";
+
+console.log("Age:", userAgeValue);
+console.log("Nickname:", userNameNickname);
