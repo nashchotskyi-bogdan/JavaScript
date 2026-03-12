9d04cad6af298d66d7b66319137fe07255a97b62
Bogdan <naszczotskyib@gmail.com>
2026-03-12 15:30:11 +0100

    2

---
+++ 2/main.js
@@ -0,0 +1,27 @@
+let result1 = (5 + 3) * 2 - 7;
+console.log('result1 = (5 + 3) * 2 - 7 =', result1);
+
+let result2 = (10 / 2) ** 2;
+console.log('result2 = (10 / 2) ** 2 =', result2);
+
+let result3 = 2 * (4 + 3) - (8 / 2);
+console.log('result3 = 2 * (4 + 3) - (8 / 2) =', result3);
+
+let result4 = (15 % 4) + 5;
+console.log('result4 = (15 % 4) + 5 =', result4);
+
+let result5 = 3 - (2 ** 4);
+console.log('result5 = 3 - (2 ** 4) =', result5);
+
+let num1 = 10;
+let num2 = 3;
+
+console.log('\n--- Додаткові операції ---');
+console.log('num1 =', num1);
+console.log('num2 =', num2);
+console.log('num1 + num2 =', num1 + num2);
+console.log('num1 - num2 =', num1 - num2);
+console.log('num1 * num2 =', num1 * num2);
+console.log('num1 / num2 =', num1 / num2);
+console.log('num1 % num2 =', num1 % num2);
+console.log('num1 ** num2 =', num1 ** num2);
